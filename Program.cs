a133b85b6ae28376ca30c1de840db730423ad850
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-23 11:45:39 -0400

    Add manual garbage collection to Main method

--- Program.cs
+++ Program.cs
@@ -13,16 +13,19 @@ namespace SimpleGC
 
             // MaxGeneration is zero based, so add 1 for display purposes.
             Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
-
             // Create a new Car object on the managed heap. We are returned a reference to this object ("refToMyCar").
             Car refToMyCar = new Car("Zippy", 50);
             // The C# dot operator (.) is used to invoke members on the object using our reference variable.
             Console.WriteLine(refToMyCar.ToString());
 
             // Print out generation of refToMyCar object.
-            Console.WriteLine("Generation of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+            Console.WriteLine("\nGeneration of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
 
+            // Force a garbage collection and wait for each object to be finalized.
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
             Console.ReadLine();
+            
         }
     }
 }
