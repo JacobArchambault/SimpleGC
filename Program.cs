31dc888cf4c130986625112cc77aca1bee4ec4cc
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-23 11:54:27 -0400

    Generate a ton of objects and test garbage collection.

--- Program.cs
+++ Program.cs
@@ -21,9 +21,29 @@ namespace SimpleGC
             // Print out generation of refToMyCar object.
             Console.WriteLine("\nGeneration of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
 
-            // Force a garbage collection and wait for each object to be finalized.
-            GC.Collect();
+            // Make a ton of objects for testing purposes.
+            object[] tonsOfObjects = new object[50000];
+            for (int i = 0; i < 50000; i++)
+                tonsOfObjects[i] = new object();
+
+            // Collect only gen 0 objects
+            GC.Collect(0, GCCollectionMode.Forced);
             GC.WaitForPendingFinalizers();
+
+            // Print out generation of refToMyCar.
+            Console.WriteLine("\nGeneration of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+
+            // See if tonsOfObjects[9000] is still alive.
+            if (tonsOfObjects[9000] != null)
+                Console.WriteLine("Generation of tonsOfObjects[9000] is: {0}", GC.GetGeneration(tonsOfObjects[9000]));
+            else
+                Console.WriteLine("tonsOfObjects[9000] is no longer alive.");
+
+            // print out how many times a generation has been swept.
+            Console.WriteLine("\nGen 0 has been swept {0} times", GC.CollectionCount(0));
+            Console.WriteLine("\nGen 1 has been swept {0} times", GC.CollectionCount(2));
+            Console.WriteLine("\nGen 2 has been swept {0} times", GC.CollectionCount(2));
+
             Console.ReadLine();
             
         }
